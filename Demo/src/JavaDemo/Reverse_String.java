9bfbf486a8ffdf4de4d4412458788a6a85719019
Prasanth Sasidharan <prasanth.s@iqzsystems.com>
2021-11-22 15:42:25 +0530

    Mycodepush

---
+++ Demo/src/JavaDemo/Reverse_String.java
@@ -0,0 +1,14 @@
+package JavaDemo;
+public class Reverse_String
+{
+public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	String Pra = "Prasanth";
+	String rev = "";
+	int len =Pra.length(); //leng=10
+	for(int i= len-1;i>0;i--) {
+	rev = rev+ Pra.charAt(i);
+	}
+	System.out.println(rev);
+	}
+}
