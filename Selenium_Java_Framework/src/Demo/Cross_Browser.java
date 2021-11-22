afd622fb3d1a02bfb38531cbd49e75079191a8bc
Prasanth Sasidharan <prasanth.s@iqzsystems.com>
2021-11-22 14:51:02 +0530

    MycodepushtoGIT

---
+++ Selenium_Java_Framework/src/Demo/Cross_Browser.java
@@ -0,0 +1,43 @@
+package Demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class Cross_Browser {
+
+    WebDriver driver = null;
+    String projectPath = System.getProperty("user.dir");
+
+    @Parameters("browserName")
+    @BeforeTest
+    public void setup(String browserName) {
+        System.out.println("Browser name is : " + browserName);
+        System.out.println("Thread ID : "+ Thread.currentThread().getId());
+
+        if (browserName.equalsIgnoreCase("chrome")) {
+            System.setProperty("webdriver.chrome.driver", projectPath+"/drivers/chromedriver.exe");
+            driver = new ChromeDriver();
+        } else if (browserName.equalsIgnoreCase("edge")) {
+            System.setProperty("webdriver.edge.driver", projectPath+"/drivers/msedgedriver.exe");
+            driver = new EdgeDriver();
+        }
+    }
+    
+    @Test
+    public void test1() throws Exception {
+    	driver.get("https://google.com");
+    	Thread.sleep(4000);
+    }
+    
+    @AfterTest
+    public void finish() {
+    	driver.quit();
+    	System.out.println("Test is successful");
+    }
+
+}
